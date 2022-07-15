356dc47fd4e77c8c5407113f953a9d2bad5487d2
Chris Duflo <cduflo@zuora.com>
2022-07-15 15:38:25 -0400

    initial find replace for reddit

---
+++ content-scripts/reddit.js
@@ -0,0 +1,99 @@
+const replacements = [
+  // Plural
+  {
+    search: ["democrats", "republicans", "conservatives", "progressives"],
+    replace: "Gov. Officials", // Source this from user option!,
+  },
+  // Singular
+  {
+    search: [
+      "democrat",
+      "republican",
+      "GOP",
+      "left-wing",
+      "right-wing",
+      "progressive",
+      "conservative",
+    ],
+    replace: "US Government", // Source this from user option!,
+  },
+];
+
+window.onload = () => findAndReplaceDOMEls();
+
+// TODO: need a good general purpose trigger when more content is loaded
+let oldXHROpen = window.XMLHttpRequest.prototype.open;
+window.XMLHttpRequest.prototype.open = function (method, url) {
+  this.addEventListener("load", function () {
+    findAndReplaceDOMEls();
+  });
+
+  return oldXHROpen.apply(this, arguments);
+};
+
+const findAndReplaceDOMEls = () => {
+  replacements.forEach(({ search, replace }) =>
+    replaceOnDocument(new RegExp(`${search.join("|")}`, "gi"), replace, {
+      attrs: [
+        "title",
+        "alt",
+        "onerror", // This will be ignored
+      ],
+      props: [
+        "value", // Changing an `<input>`’s `value` attribute won’t change its current value, so the property needs to be accessed here
+      ],
+    })
+  );
+};
+
+// Credit: https://stackoverflow.com/a/41886794/6136828
+const replaceOnDocument = (() => {
+  console.log("FINDING AND REPLACING");
+
+  const replacer = {
+    [document.TEXT_NODE](node, pattern, string) {
+      node.textContent = node.textContent.replace(pattern, string);
+    },
+    [document.ELEMENT_NODE](node, pattern, string, { attrs, props } = {}) {
+      attrs.forEach((attr) => {
+        if (typeof node[attr] !== "function" && node.hasAttribute(attr)) {
+          node.setAttribute(
+            attr,
+            node.getAttribute(attr).replace(pattern, string)
+          );
+        }
+      });
+      props.forEach((prop) => {
+        if (typeof node[prop] === "string" && node.hasAttribute(prop)) {
+          node[prop] = node[prop].replace(pattern, string);
+        }
+      });
+    },
+  };
+
+  return (
+    pattern,
+    string,
+    {
+      target = document.body,
+      attrs: [...attrs] = [],
+      props: [...props] = [],
+    } = {}
+  ) => {
+    // Handle `string` — see the last section
+    [
+      target,
+      ...[
+        target,
+        ...target.querySelectorAll("*:not(script):not(noscript):not(style)"),
+      ].flatMap(({ childNodes: [...nodes] }) => nodes),
+    ]
+      .filter(({ nodeType }) => replacer.hasOwnProperty(nodeType))
+      .forEach((node) =>
+        replacer[node.nodeType](node, pattern, string, {
+          attrs,
+          props,
+        })
+      );
+  };
+})();
